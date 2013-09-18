14bf0cead1fea959eac6bc21fad2ab9f424d9f0c
Investigation <test@test>
2013-09-18 21:20:07 +0300

    Investigation

--- OsmSharp.WinForms.UI.Sample/MapControlForm.cs
+++ OsmSharp.WinForms.UI.Sample/MapControlForm.cs
@@ -42,6 +42,8 @@ using OsmSharp.UI;
 using OsmSharp.Routing.Graph.Router;
 using OsmSharp.Routing.CH.PreProcessing;
 using OsmSharp.UI.Renderer.Scene;
+using OsmSharp.Data.SQLServer.Osm;
+using System.Data.SqlClient;
 
 namespace OsmSharp.WinForms.UI.Sample
 {
@@ -82,15 +84,23 @@ namespace OsmSharp.WinForms.UI.Sample
                 Assembly.GetExecutingAssembly().GetManifestResourceStream("OsmSharp.WinForms.UI.Sample.test.mapcss"),
                 imageSource);
 
+
+            const string connectionString = @"Server=localhost;Database=osmsharp;User Id=sa;Password=sa123;";
+
+            var _dbConnection = new SqlConnection(connectionString);
+            _dbConnection.Open();
+            var sqlSource = new SQLServerDataSource(_dbConnection);
+
             // initialize the data source.
             //var dataSource = MemoryDataSource.CreateFromXmlStream(new FileInfo(@"c:\OSM\bin\wechel.osm").OpenRead());
-            var dataSource = MemoryDataSource.CreateFromPBFStream(new FileInfo(@"c:\OSM\bin\wvl.osm.pbf").OpenRead());
+            //var dataSource = MemoryDataSource.CreateFromXmlStream(new FileInfo(@"c:\ms\RU-KGD.osm").OpenRead());
+          //MemoryDataSource.
             //var dataSource = MemoryDataSource.CreateFromPBFStream(new FileInfo(@"c:\OSM\bin\lebbeke.osm.pbf").OpenRead());
             //Assembly.GetExecutingAssembly().GetManifestResourceStream("OsmSharp.WinForms.UI.Sample.test.osm"));
 
             // initialize map.
             var map = new OsmSharp.UI.Map.Map();
-            map.AddLayer(new OsmLayer(dataSource, mapCSSInterpreter, map.Projection));
+            map.AddLayer(new OsmLayer(sqlSource, mapCSSInterpreter, map.Projection));
             //map.AddLayer(new LayerTile(@"http://otile1.mqcdn.com/tiles/1.0.0/osm/{0}/{1}/{2}.png"));
             //map.AddLayer(new LayerMBTile(@"C:\Users\xivk\Documents\Nostalgeo.mbtiles"));
             //map.AddLayer(
@@ -107,15 +117,15 @@ namespace OsmSharp.WinForms.UI.Sample
             //gpxLayer.AddGpx(
             //    Assembly.GetExecutingAssembly().GetManifestResourceStream("OsmSharp.WinForms.UI.Sample.test.gpx"));
             //map.AddLayer(gpxLayer);
-
+          //map.
             // set control properties.
             this.mapControl1.Map = map;
             //this.mapControl1.Center = new GeoCoordinate(51.26371, 4.7854); //51.26371&lon=4.7854 // wechel.osm
             //this.mapControl1.Center = new GeoCoordinate(50.88672, 3.23899); // lendelede
             //this.mapControl1.Center = new GeoCoordinate(50.9523195, 3.0997436);
-            this.mapControl1.Center = new GeoCoordinate(51.156803, 2.958887); //50.9969&lon=4.1201
+            this.mapControl1.Center = new GeoCoordinate(54.65725, 20.48453); //50.9969&lon=4.1201
             //this.mapControl1.Center = new GeoCoordinate(50.9969, 4.1201);
-            this.mapControl1.ZoomLevel = 19;
+            this.mapControl1.ZoomLevel = 15;
         }
 
         #region Routing Tests
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsmSharp.Osm.Data.Xml.Processor;
+using System.Reflection;
+using System.Data.SqlClient;
+using System.Data;
+using OsmSharp.Data.SQLServer.Osm.SchemaTools;
+using OsmSharp.Data.SQLServer.Osm.Streams;
+
+namespace UnitTestProject2
+{
+  [TestClass]
+  public class UnitTest1
+  {
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+      var source = new XmlOsmStreamSource(@"C:\ms\RU-KGD.osm");
+
+      const string connectionString = @"Server=localhost;Database=fcsdbo;User Id=sa;Password=sa123;";
+
+      var dbConnection = new SqlConnection(connectionString);
+      dbConnection.Open();
+      SQLServerSchemaTools.Remove(dbConnection);
+      SQLServerSchemaTools.CreateAndDetect(dbConnection);
+      SQLServerSchemaTools.AddConstraints(dbConnection);
+
+      var target = new SQLServerOsmStreamTarget(dbConnection);
+      target.RegisterSource(source);
+      target.Pull();
+    }
+  }
+}
